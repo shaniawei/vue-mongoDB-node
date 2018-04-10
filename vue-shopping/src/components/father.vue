ca70e4630ae3526b8c3065942286ff7b7f9f77d2
shaniawei <1161687742@qq.com>
2018-04-10 18:43:56 +0800

    test

---
+++ vue-shopping/src/components/father.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="father">
+    <img src="../assets/logo.png">
+    <HelloWorld num='10'></HelloWorld>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+    import HelloWorld from './HelloWorld'
+    export default {
+        name: 'father',
+        data () {
+            return {
+
+            }
+        },
+        components:{
+            HelloWorld
+        }
+    }
+</script>
+
+<style scoped>
+   
+</style>
+
