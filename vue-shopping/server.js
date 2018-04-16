2a269678162308bfe303b0ddc65b40fb772d5712
shaniawei <1161687742@qq.com>
2018-04-16 12:57:41 +0800

    node server

--- vue-shopping/server.js
+++ vue-shopping/server.js
@@ -1,29 +1,43 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+var express=require('express')
+var bodyParser=require('body-parser')
 
-var app = new express()
+var fs=require('fs')
 
-var PORT = process.env.PORT || 3002
+var app=new express()
+
+var router = express.Router()
+
+var PORT=process.env.PORT||3002
 
 app.use(express.static('./public'))
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }))
 app.use(bodyParser.json())
 
-app.get('/person', (req, res) => {
-    // res.json({
-    //     name: 'wmy',
-    //     age: 15
-    // })
-    res.end('hahahah')
+app.post('/save',(req,res)=>{
+	console.log(req.body)
+	res.end('保存成功')
 })
 
-app.post('/save', (req, res) => {
-    console.log(req.body)
-    res.end('保存成功')
+app.get('/test.html',(req,res)=>{
+	var cnt=fs.readFileSync('./test.html')
+	res.end(cnt)
 })
 
-app.listen(PORT, (err) => {
-    console.log(`server is running in 127.0.0.1:${PORT}`)
+app.listen(PORT,(err)=>{
+	console.log(`server is running in 127.0.0.1:${PORT}`)
+})
+
+app.get('/person',(req,res)=>{
+	var query=req.query||{}
+	var cb=query.cb||query.callback
+	// res.type('text/javascript')
+	var jsonp=`!function(){
+		${cb}({
+			name:'wmy',
+			age:15
+		})
+	}()`
+	res.end(jsonp)
 })
